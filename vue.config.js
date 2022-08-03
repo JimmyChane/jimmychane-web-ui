4ea22aceb7aba09195fa6313bfb19ed23488941c
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2022-08-04 00:00:50 +0800

    expertise changes

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+   transpileDependencies: true,
+   // css: {
+   //    extract: {
+   //       filename: `./css/style.css`,
+   //       chunkFilename: `./css/style.chunk.css`,
+   //    },
+   // },
+   // configureWebpack: (config) => {
+   //    config.output.filename = `./js/app.js`;
+   //    config.output.chunkFilename = `./js/app.chunk.js`;
+   // },
+});
