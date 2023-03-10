dd1c21a758123d03eb8e4e13303314005f74a529
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2023-03-10 23:20:47 +0800

    Update main.router.js

--- src/main.router.js
+++ src/main.router.js
@@ -6,16 +6,16 @@ import {
 } from "vue-router";
 
 const router = createRouter({
-   history: createWebHistory(),
+   history: createWebHashHistory(),
    routes: [
       { name: "home", path: "/", component: () => import("./PageHome.vue") },
-      // { name: "code", path: "/code", component: () => import("./PageCode.vue") },
-      // { name: "art", path: "/art", component: () => import("./PageArt.vue") },
-      // {
-      //    name: "fursona",
-      //    path: "/fursona",
-      //    component: () => import("./PageFursona.vue"),
-      // },
+      { name: "code", path: "/code", component: () => import("./PageCode.vue") },
+      { name: "art", path: "/art", component: () => import("./PageArt.vue") },
+      {
+         name: "fursona",
+         path: "/fursona",
+         component: () => import("./PageFursona.vue"),
+      },
 
       { path: "/:pathMatch(.*)*", redirect: { path: "/" } },
    ],
