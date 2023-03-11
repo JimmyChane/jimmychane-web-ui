938947f715a62a847213f92fa9807dc42732afcb
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2023-03-12 00:06:54 +0800

    Refactor

---
+++ src/App-Actionbar.vue
@@ -0,0 +1,48 @@
+<script>
+   import AppNav from "./App-Nav.vue";
+   export default { components: { AppNav } };
+</script>
+
+<template>
+   <div class="App-actionbar">
+      <div class="App-actionbar-items">
+         <AppNav path="/" title="Home" />
+         <AppNav path="/code" title="Code" />
+         <!-- <AppNav path="/art" title="Art" />
+            <AppNav path="/fursona" title="Fursona" /> -->
+      </div>
+   </div>
+</template>
+
+<style lang="scss" scoped>
+   .App-actionbar {
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+
+      position: sticky;
+      top: 0;
+
+      background-color: hsla(0, 0%, 100%, 0.5);
+      --blur: 1rem;
+      @supports (-webkit-backdrop-filter: none) or (backdrop-filter: none) {
+         -webkit-backdrop-filter: blur(var(--blur));
+         backdrop-filter: blur(var(--blur));
+      }
+
+      .App-actionbar-items {
+         gap: 1.2rem;
+         padding: 0.5rem;
+         padding: 1rem;
+
+         display: flex;
+         flex-direction: row;
+         justify-content: flex-start;
+         align-items: center;
+
+         overflow-y: auto;
+      }
+   }
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,17 @@
 <script>
-   import AppNav from "./App-Nav.vue";
+   import Actionbar from "./App-Actionbar.vue";
 
    export default {
-      components: { AppNav },
+      components: { Actionbar },
       data: (c) => ({ scrollTop: 0 }),
-      mounted() {
-         window.addEventListener("resize", this.onResize);
-         this.onResize();
-      },
-      unmounted() {
-         window.removeEventListener("resize", this.onResize);
-      },
-      methods: {
-         onResize() {
-            const vh = window.innerHeight * 0.01;
-            document.body.style.setProperty("--vh", `${vh}px`);
-         },
-      },
    };
 </script>
 
 <template>
    <div class="App" @scroll="(event) => (scrollTop = event.target.scrollTop)">
-      <div class="App-actionbar">
-         <div class="App-actionbar-items">
-            <AppNav path="/" title="Home" />
-            <AppNav path="/code" title="Code" />
-            <!-- <AppNav path="/art" title="Art" />
-            <AppNav path="/fursona" title="Fursona" /> -->
-         </div>
-      </div>
+      <Actionbar style="z-index: 2" />
 
-      <div class="App-body">
+      <div class="App-body" style="z-index: 1">
          <router-view class="App-Page" />
       </div>
    </div>
@@ -45,6 +25,8 @@
       -moz-osx-font-smoothing: grayscale;
       color: black;
 
+      background: linear-gradient(140.09deg, #e0f7f2 10.49%, #f6dbdb 90.41%);
+
       width: 100%;
       min-height: 100dvh;
       flex-grow: 1;
@@ -56,38 +38,7 @@
 
       position: relative;
 
-      .App-actionbar {
-         z-index: 2;
-         width: 100%;
-         display: flex;
-         flex-direction: row;
-         justify-content: center;
-         align-items: center;
-
-         position: sticky;
-         top: 0;
-
-         background-color: hsla(0, 0%, 100%, 0.5);
-         @supports (-webkit-backdrop-filter: none) or (backdrop-filter: none) {
-            -webkit-backdrop-filter: blur(2rem);
-            backdrop-filter: blur(2rem);
-         }
-
-         .App-actionbar-items {
-            gap: 1.2rem;
-            padding: 0.5rem;
-            padding: 1rem;
-
-            display: flex;
-            flex-direction: row;
-            justify-content: flex-start;
-            align-items: center;
-
-            overflow-y: auto;
-         }
-      }
       .App-body {
-         z-index: 1;
          flex-grow: 1;
          display: flex;
          flex-direction: column;
