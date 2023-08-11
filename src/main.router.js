cf0e60e7e88b99ddcffcd0c584a51d2e5150be4a
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2023-08-11 14:10:26 +0800

    Optimize
    - optimize font family
    - changed web from (hash history) to (history)

--- src/main.router.js
+++ src/main.router.js
@@ -3,30 +3,34 @@ import {
   RouteRecordRaw,
   createWebHashHistory,
   createWebHistory,
+  RouterOptions,
 } from "vue-router";
 
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes: [
-    {
-      name: "home",
-      path: "/",
-      component: () => import("./views/home/PageHome.vue"),
-    },
-    {
-      name: "code",
-      path: "/code",
-      component: () => import("./views/code/PageCode.vue"),
-    },
-    // { name: "art", path: "/art", component: () => import("./PageArt.vue") },
-    {
-      name: "fursona",
-      path: "/fursona",
-      component: () => import("./views/fursona/PageFursona.vue"),
-    },
+const routes = [
+  {
+    name: "home",
+    path: "/",
+    component: () => import("./views/home/PageHome.vue"),
+  },
+  {
+    name: "code",
+    path: "/code",
+    component: () => import("./views/code/PageCode.vue"),
+  },
+  // { name: "art", path: "/art", component: () => import("./PageArt.vue") },
+  {
+    name: "fursona",
+    path: "/fursona",
+    component: () => import("./views/fursona/PageFursona.vue"),
+  },
 
-    { path: "/:pathMatch(.*)*", redirect: { path: "/" } },
-  ],
+  { path: "/:pathMatch(.*)*", redirect: { path: "/" } },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  // history: createWebHashHistory(),
+  routes,
 });
 
 export default router;
