11d8dabdb982fb9e91950721853e8d38f478034d
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2026-02-20 03:34:57 +0800

    update package

--- src/app/route-loading/App-RouteLoading.vue
+++ src/app/route-loading/App-RouteLoading.vue
@@ -1,78 +1,21 @@
 <script setup lang="ts">
-import { waitMs } from '@chanzor/utils';
-import { waitFrameMs } from '@chanzor/vue-utils';
-import {
-  type CubicBezierPoints,
-  type EasingFunction,
-  TransitionPresets,
-  computedAsync,
-  useTransition,
-} from '@vueuse/core';
+import { useLoadingAnimate } from '@chanzor/vue-use';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
 
-let updateTime = 0;
-
-const duration = ref(0);
-const transition = ref<CubicBezierPoints | EasingFunction>(TransitionPresets.easeInOutCubic);
-const percentageSource = ref(0);
-const percentageOutput = useTransition(percentageSource, { duration, transition });
-
-const opacity = computedAsync(async () => {
-  if (percentageSource.value === 1) {
-    await waitMs(duration.value + opacityDelay.value);
-    return 0;
-  }
-
-  return 1;
-}, 0);
-const opacityDelay = ref(500);
+const { opacity, percentageOutput, animateStart, animateEnd } = useLoadingAnimate();
 
 const isRouteBeforeOnce = ref(false);
 const isRouteWatchOnce = ref(false);
 
-const animateStartRoute = async () => {
-  const now = (updateTime = Date.now());
-
-  await waitFrameMs();
-  if (now !== updateTime) return;
-
-  if (percentageOutput.value < 1) {
-    duration.value = 300;
-    percentageSource.value = 0;
-
-    await waitMs(duration.value);
-    if (now !== updateTime) return;
-  } else {
-    duration.value = 0;
-    percentageSource.value = 0;
-  }
-
-  await waitFrameMs();
-  if (now !== updateTime) return;
-
-  transition.value = TransitionPresets.easeOutCubic;
-  duration.value = 3000;
-  percentageSource.value = 0.8;
-};
-const animateEndRoute = async () => {
-  const now = (updateTime = Date.now());
-
-  await waitFrameMs();
-  if (now !== updateTime) return;
-
-  transition.value = TransitionPresets.easeInOutCubic;
-  duration.value = 700;
-  percentageSource.value = 1;
-};
-
+// TODO: fix, potential route each back and forward
 router.beforeEach((_to, _from, next) => {
   if (!isRouteBeforeOnce.value) {
     isRouteBeforeOnce.value = true;
   } else {
-    animateStartRoute();
+    animateStart();
   }
 
   next();
@@ -81,7 +24,7 @@ router.afterEach(() => {
   if (!isRouteWatchOnce.value) {
     isRouteWatchOnce.value = true;
   } else {
-    animateEndRoute();
+    animateEnd();
   }
 });
 </script>
